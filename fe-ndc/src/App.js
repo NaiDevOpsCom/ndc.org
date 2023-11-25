b2f0ce1a8932b603432e1debdc37e1de3ff6a094
IvanNapsterKE <nguyoivan@gmail.com>
2023-11-25 15:19:21 +0300

    Updated App.js

--- fe-ndc/src/App.js
+++ fe-ndc/src/App.js
@@ -1,15 +1,32 @@
-import logo from './assets/logos/ndclogo1.png';
-
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Careers from "./components/Careers";
+import Events from "./components/Events";
+import Sponsors from "./sections/Sponsors";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <img
-            src={logo}
-            alt="logo"
-            className="w-40 md:cursor-pointer"
-          />
-      <h1 className="text-6xl text-blue-500 font-bold underline"> NDC </h1>
+      {/* navbar import begins */}
+      <Navbar />
+      {/* navbar import ends */}
+      <div className=" min-h-screen">
+        {/* Imports begin */}
+        <Homepage />
+        <About />
+        <Blog />
+        <Careers />
+        <Events />
+        <Sponsors />
+        {/* import end */}
+      </div>
+
+      {/* footer import begins */}
+      <Footer />
+      {/* footer import ends */}
     </div>
   );
 }
